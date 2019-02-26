8ca271eeb76d58f84028c62b4fe43753555efc38
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-02-25 23:49:50 -0700

    j

--- testbench.cu
+++ testbench.cu
@@ -9,11 +9,11 @@ __global__ void addVec(int *a, size_t pitch_a, int *b, size_t pitch_b, int *c, s
 {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
-  if (i < Nrow && j < Ncol)
+  if (i < Ncol && j < Nrow)
   {
-    int* aElem = (int*)((char*)a + i * pitch_a) + j;
-    int* bElem = (int*)((char*)b + i * pitch_b) + j;
-    int* cElem = (int*)((char*)c + i * pitch_c) + j;
+    int* aElem = (int*)((char*)a + j * pitch_a) + i;
+    int* bElem = (int*)((char*)b + j * pitch_b) + i;
+    int* cElem = (int*)((char*)c + j * pitch_c) + i;
     *cElem = *aElem + *bElem;
   }
 }
@@ -62,8 +62,8 @@ int main() {
   const unsigned NUM_BLOCKS_COL =
       (Ncol + THREADS_PER_BLOCK_COL - 1) / THREADS_PER_BLOCK_COL;
 
-  dim3 gridDim(NUM_BLOCKS_ROW, NUM_BLOCKS_COL);
-  dim3 blockDim(THREADS_PER_BLOCK_ROW, THREADS_PER_BLOCK_COL);
+  dim3 gridDim(NUM_BLOCKS_COL, NUM_BLOCKS_ROW);
+  dim3 blockDim(THREADS_PER_BLOCK_COL, THREADS_PER_BLOCK_ROW);
   addVec<<<gridDim, blockDim>>>(d_a, pitch_a, d_b, pitch_b, d_c, pitch_c, Nrow, Ncol);
 
   int c[Nrow][Ncol];
